8ec64fd565f257b6389f46cea3804678faf5f73c
vjt4 <54646079+vjt4@users.noreply.github.com>
2026-01-11 10:45:20 -0600

    Context template custom github

---
+++ context-based-templates-data/SAMGrid.lua
@@ -0,0 +1,40 @@
+---@type ContextBasedTemplate
+Template = {
+    Name = "SAMGrid",
+    TriggersOnLand = true,
+    TemplateSortingOrder = 12,
+    TemplateData = {
+        3,
+        3,
+        {
+            'ueb2304',
+            7210,
+            0,
+            0
+        },
+        {
+            'ueb2304',
+            7611,
+            7,
+            0
+        },
+        {
+            'ueb2304',
+            7664,
+            0,
+            -7
+        },
+        {
+            'ueb2304',
+            7768,
+            -7,
+            0
+        },
+        {
+            'ueb2304',
+            7827,
+            0,
+            7
+        }
+    },
+}
